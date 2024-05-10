ad708fbb4e93de5292d03c0cd203c4110f33e0b5
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-05-10 14:27:43 +0700

    migrate database

--- config/db.go
+++ config/db.go
@@ -7,8 +7,8 @@ import (
 )
 
 func GetConnection() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/db_wkm?parseTime=true")
-	// db, err := sql.Open("mysql", "root2:root2@tcp(192.168.70.30:3306)/db_wkm?parseTime=true")
+	// db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/db_wkm?parseTime=true")
+	db, err := sql.Open("mysql", "root2:root2@tcp(192.168.70.30:3306)/db_wkm?parseTime=true")
 	if err != nil {
 		fmt.Println(err)
 	}
