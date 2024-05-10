f73e03254ac3c29720564aaef1053de6cb307007
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-05-10 14:21:14 +0700

    fix

--- config/db.go
+++ config/db.go
@@ -22,8 +22,8 @@ func GetConnection() *sql.DB {
 }
 
 func GetConnectionAsuransi() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/asuransi?parseTime=true")
-	// db, err := sql.Open("mysql", "root2:root2@tcp(192.168.70.30:3306)/db_wkm?parseTime=true")
+	// db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/asuransi?parseTime=true")
+	db, err := sql.Open("mysql", "root2:root2@tcp(192.168.70.30:3306)/asuransi?parseTime=true")
 	if err != nil {
 		fmt.Println(err)
 	}
