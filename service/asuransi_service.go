fc9f903ca0c9587e91179c85a1ff320826842780
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-09-02 10:39:09 +0700

    fix

--- service/asuransi_service.go
+++ service/asuransi_service.go
@@ -376,6 +376,78 @@ func (s *asuransiService) ExportReport1(tgl1 string, tgl2 string) {
 }
 
 func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
+	xlsx := excelize.NewFile()
+
+	headerStyle, _ := xlsx.NewStyle(`{
+		"font": {
+			"bold": true
+		},
+		"alignment": {
+	        "horizontal": "center",
+	        "vertical": "center",
+			"wrap_text": true
+	    },
+		"fill": {
+			"type": "pattern",
+			"color": [
+				"#FFFF00"
+			],
+			"pattern": 1
+		},
+		"border": [
+			{
+				"type": "left",
+				"style": 1,
+				"color": "#000000"
+			},
+			{
+				"type": "top",
+				"style": 1,
+				"color": "#000000"
+			},
+			{
+				"type": "right",
+				"style": 1,
+				"color": "#000000"
+			},
+			{
+				"type": "bottom",
+				"style": 1,
+				"color": "#000000"
+			}
+		]
+	}`)
+	borderStyle, err := xlsx.NewStyle(`{
+		"alignment": {
+	        "horizontal": "center",
+	        "vertical": "center"
+	    },
+		"border": [
+			{
+				"type": "left",
+				"style": 1,
+				"color": "#000000"
+			},
+			{
+				"type": "top",
+				"style": 1,
+				"color": "#000000"
+			},
+			{
+				"type": "right",
+				"style": 1,
+				"color": "#000000"
+			},
+			{
+				"type": "bottom",
+				"style": 1,
+				"color": "#000000"
+			}
+		]
+	}`)
+	if err != nil {
+		fmt.Println("ini error style ", err)
+	}
 
 	startDate, _ := time.Parse("2006-01-02", tgl1)
 	endDate, _ := time.Parse("2006-01-02", tgl2)
@@ -390,7 +462,6 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 	masterPending := s.trR.MasterAlasanPending()
 	masterAlasanTdkBerminat := s.trR.MasterAlasanTdkBerminat()
 
-	xlsx := excelize.NewFile()
 	startRow := 4
 	rekapSheet := "Rekap"
 	xlsx.SetSheetName(xlsx.GetSheetName(1), rekapSheet)
@@ -401,8 +472,14 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 	xlsx.SetCellValue(rekapSheet, "D3", "Tidak Berminat")
 	xlsx.SetCellValue(rekapSheet, "E3", "Berminat")
 	xlsx.SetCellValue(rekapSheet, "F3", "Total")
+	xlsx.SetCellStyle(rekapSheet, "A3", "F3", headerStyle)
+	xlsx.SetColWidth(rekapSheet, "A", "F", 17)
+	xlsx.MergeCell(rekapSheet, "A1", "G1")
+	xlsx.SetCellValue(rekapSheet, "A1", "Report Asuransi Periode "+tgl1+" - "+tgl2)
+	xlsx.SetCellStyle(rekapSheet, "A1", "G1", headerStyle)
 
 	startMergeRekap := startRow
+	awalBorder := startRow
 	for index, value := range rekapSourceInfo {
 		currentSource := "Excell"
 		if value["jenis_source"] == "W" {
@@ -426,6 +503,7 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 		xlsx.SetCellValue(rekapSheet, fmt.Sprintf("F%d", startRow), value["total"])
 		startRow += 1
 	}
+	xlsx.SetCellStyle(rekapSheet, fmt.Sprintf("A%d", awalBorder), fmt.Sprintf("F%d", startRow-1), borderStyle)
 
 	startRow += 1
 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("A%d", startRow), "Id User")
@@ -436,8 +514,11 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("F%d", startRow), "Berminat")
 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("G%d", startRow), "Total")
 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("H%d", startRow), "Source Info")
+	xlsx.SetCellStyle(rekapSheet, fmt.Sprintf("A%d", startRow), fmt.Sprintf("H%d", startRow), headerStyle)
+
 	startRow += 1
 	startMergeRekap = startRow
+	awalBorder = startRow
 	for index, value := range rekapKdUser {
 		user := s.uR.FindByUsername(value["kd_user"].(string))
 		if user.ID == 0 {
@@ -465,18 +546,23 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 		xlsx.SetCellValue(rekapSheet, fmt.Sprintf("H%d", startRow), user.DataSource)
 		startRow += 1
 	}
+	xlsx.SetCellStyle(rekapSheet, fmt.Sprintf("A%d", awalBorder), fmt.Sprintf("H%d", startRow-1), borderStyle)
 
 	pendingSheet := "Pending"
 	xlsx.NewSheet(pendingSheet)
+	xlsx.MergeCell(pendingSheet, "A1", "G1")
+	xlsx.SetCellValue(pendingSheet, "A1", "Report Asuransi Periode "+tgl1+" - "+tgl2)
+	xlsx.SetCellStyle(pendingSheet, "A1", "G1", headerStyle)
+
 	startRow = 4
 	startMergeRekap = startRow
-
 	xlsx.SetCellValue(pendingSheet, "A3", "Alasan")
 	startCol := 1
 	for d := startDate; d.Before(endDate) || d.Equal(endDate); d = d.AddDate(0, 1, 0) {
 		xlsx.SetCellValue(pendingSheet, fmt.Sprintf("%s3", listCol[startCol]), bulan[d.Month()])
 		startCol += 1
 	}
+	xlsx.SetCellStyle(pendingSheet, "A3", fmt.Sprintf("%s3", listCol[startCol-1]), headerStyle)
 	for _, value := range rekapBulanAlasanPending {
 		startCol = 1
 		kodeAlasanPending, _ := strconv.Atoi(value["alasan_pending"].(string))
@@ -488,6 +574,8 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 		}
 		startRow += 1
 	}
+	xlsx.SetCellStyle(pendingSheet, "A4", fmt.Sprintf("%s%d", listCol[startCol-1], startRow-1), borderStyle)
+	xlsx.SetColWidth(pendingSheet, "A", listCol[startCol-1], 17)
 
 	startRow += 1
 	xlsx.SetCellValue(pendingSheet, fmt.Sprintf("A%d", startRow), "Id User")
@@ -498,7 +586,11 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 		xlsx.SetCellValue(pendingSheet, fmt.Sprintf("%s%d", listCol[startCol], startRow), value.Nama)
 		startCol += 1
 	}
+	xlsx.SetCellStyle(pendingSheet, fmt.Sprintf("A%d", startRow), fmt.Sprintf("%s%d", listCol[startCol-1], startRow), headerStyle)
+	xlsx.SetColWidth(pendingSheet, "A", listCol[startCol-1], 17)
+
 	startRow += 1
+	awalBorder = startRow
 	startMergeRekap = startRow
 	for index, value := range rincianBulanAlasanPending {
 		user := s.uR.FindByUsername(value["kd_user"].(string))
@@ -523,20 +615,28 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 			startMergeRekap = startRow + 1
 		}
 		startRow += 1
-
 	}
+	xlsx.SetCellStyle(pendingSheet, fmt.Sprintf("A%d", awalBorder), fmt.Sprintf("%s%d", listCol[startCol-1], startRow-1), borderStyle)
+	xlsx.SetColWidth(pendingSheet, "A", listCol[startCol-1], 17)
 
 	tdkBerminatSheet := "Tidak Berminat"
 	xlsx.NewSheet(tdkBerminatSheet)
-	startRow = 4
-	startMergeRekap = startRow
+	xlsx.MergeCell(tdkBerminatSheet, "A1", "G1")
+	xlsx.SetCellValue(tdkBerminatSheet, "A1", "Report Asuransi Periode "+tgl1+" - "+tgl2)
+	xlsx.SetCellStyle(tdkBerminatSheet, "A1", "G1", headerStyle)
 
+	startRow = 4
+	awalBorder = startRow
+	startMergeRekap = startRow
 	xlsx.SetCellValue(tdkBerminatSheet, "A3", "Alasan")
 	startCol = 1
 	for d := startDate; d.Before(endDate) || d.Equal(endDate); d = d.AddDate(0, 1, 0) {
 		xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("%s3", listCol[startCol]), bulan[d.Month()])
 		startCol += 1
 	}
+	xlsx.SetCellStyle(tdkBerminatSheet, "A3", fmt.Sprintf("%s3", listCol[startCol-1]), headerStyle)
+	xlsx.SetColWidth(tdkBerminatSheet, "A", listCol[startCol-1], 17)
+
 	for _, value := range rekapBulanAlasanTdkBerminat {
 		startCol = 1
 		kodeAlasanTdkBerminat, _ := strconv.Atoi(value["alasan_tdk_berminat"].(string))
@@ -548,6 +648,7 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 		}
 		startRow += 1
 	}
+	xlsx.SetCellStyle(tdkBerminatSheet, fmt.Sprintf("A%d", awalBorder), fmt.Sprintf("%s%d", listCol[startCol-1], startRow-1), borderStyle)
 
 	startRow += 1
 	xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("A%d", startRow), "Id User")
@@ -558,8 +659,12 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 		xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("%s%d", listCol[startCol], startRow), value.Nama)
 		startCol += 1
 	}
+	xlsx.SetCellStyle(tdkBerminatSheet, fmt.Sprintf("A%d", startRow), fmt.Sprintf("%s%d", listCol[startCol-1], startRow), headerStyle)
+	xlsx.SetColWidth(tdkBerminatSheet, "A", listCol[startCol-1], 17)
+
 	startRow += 1
 	startMergeRekap = startRow
+	awalBorder = startRow
 	for index, value := range rincianBulanAlasanTdkBerminat {
 		user := s.uR.FindByUsername(value["kd_user"].(string))
 		xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("A%d", startRow), user.ID)
@@ -583,214 +688,10 @@ func (s *asuransiService) ExportReport(tgl1 string, tgl2 string) {
 			startMergeRekap = startRow + 1
 		}
 		startRow += 1
-
 	}
-	// xlsx.SetCellValue(rekapSheet, "B7", "Nama")
-	// xlsx.SetCellValue(rekapSheet, "C7", "Pending")
-	// xlsx.SetCellValue(rekapSheet, "D7", "Tidak Berminat")
-	// xlsx.SetCellValue(rekapSheet, "E7", "Berminat")
-	// xlsx.SetCellValue(rekapSheet, "F7", "Total")
-	// xlsx.SetCellValue(rekapSheet, "G7", "Source Info")
+	xlsx.SetCellStyle(tdkBerminatSheet, fmt.Sprintf("A%d", awalBorder), fmt.Sprintf("%s%d", listCol[startCol-1], startRow-1), borderStyle)
 
-	// xlsx.SetCellValue(pendingSheet, "A3", "Alasan")
-	// xlsx.SetCellValue(pendingSheet, "B3", "Total")
-	// xlsx.SetColWidth(pendingSheet, "A", "A", 20)
-	// xlsx.SetColWidth(pendingSheet, "B", "G", 11)
-
-	// xlsx.SetCellValue(tdkBerminatSheet, "A3", "Alasan")
-	// xlsx.SetCellValue(tdkBerminatSheet, "B3", "Total")
-	// xlsx.SetColWidth(tdkBerminatSheet, "A", "A", 20)
-	// xlsx.SetColWidth(tdkBerminatSheet, "B", "G", 11)
-
-	// headerStyle, err := xlsx.NewStyle(`{
-	// 	"font": {
-	// 		"bold": true
-	// 	},
-	// 	"alignment": {
-	//         "horizontal": "center",
-	//         "vertical": "center"
-	//     },
-	// 	"fill": {
-	// 		"type": "pattern",
-	// 		"color": [
-	// 			"#FFFF00"
-	// 		],
-	// 		"pattern": 1
-	// 	},
-	// 	"border": [
-	// 		{
-	// 			"type": "left",
-	// 			"style": 1,
-	// 			"color": "#000000"
-	// 		},
-	// 		{
-	// 			"type": "top",
-	// 			"style": 1,
-	// 			"color": "#000000"
-	// 		},
-	// 		{
-	// 			"type": "right",
-	// 			"style": 1,
-	// 			"color": "#000000"
-	// 		},
-	// 		{
-	// 			"type": "bottom",
-	// 			"style": 1,
-	// 			"color": "#000000"
-	// 		}
-	// 	]
-	// }`)
-	// if err != nil {
-	// 	fmt.Println("ini error style ", err)
-	// }
-	// styleBorder, err := xlsx.NewStyle(`{
-	// 	"border": [
-	//     {
-	//         "type": "left",
-	//         "style": 1,
-	//         "color": "#000000"
-	//     },
-	//     {
-	//         "type": "top",
-	//         "style": 1,
-	//         "color": "#000000"
-	//     },
-	//     {
-	//         "type": "right",
-	//         "style": 1,
-	//         "color": "#000000"
-	//     },
-	//     {
-	//         "type": "bottom",
-	//         "style": 1,
-	//         "color": "#000000"
-	// 	}
-	// ]
-	// }`)
-	// if err != nil {
-	// 	fmt.Println("ini error style ", err)
-	// }
-
-	// xlsx.SetCellStyle(rekapSheet, "A1", "G1", headerStyle)
-	// xlsx.SetCellStyle(pendingSheet, "A1", "G1", headerStyle)
-	// xlsx.SetCellStyle(tdkBerminatSheet, "A1", "G1", headerStyle)
-
-	// xlsx.SetCellStyle(rekapSheet, "A3", "E3", headerStyle)
-	// xlsx.SetCellStyle(pendingSheet, "A3", "B3", headerStyle)
-	// xlsx.SetCellStyle(tdkBerminatSheet, "A3", "B3", headerStyle)
-
-	// xlsx.SetCellStyle(rekapSheet, "A4", "A5", headerStyle)
-	// xlsx.SetCellStyle(rekapSheet, "A7", "G7", headerStyle)
-	// xlsx.SetCellStyle(rekapSheet, "B4", "E5", styleBorder)
-	// xlsx.MergeCell(rekapSheet, "A1", "G1")
-	// xlsx.MergeCell(pendingSheet, "A1", "G1")
-	// xlsx.MergeCell(tdkBerminatSheet, "A1", "G1")
-
-	// xlsx.SetCellValue(rekapSheet, "A1", "Report Asuransi Periode "+tgl1+" - "+tgl2)
-	// xlsx.SetCellValue(pendingSheet, "A1", "Report Asuransi Periode "+tgl1+" - "+tgl2)
-	// xlsx.SetCellValue(tdkBerminatSheet, "A1", "Report Asuransi Periode "+tgl1+" - "+tgl2)
-
-	// jenis_source := ""
-	// for i, each := range rekapSourceInfo {
-	// 	if each["jenis_source"] == "W" {
-	// 		jenis_source = "Wanda"
-	// 	}
-	// 	if each["jenis_source"] == "E" {
-	// 		jenis_source = "Excel"
-	// 	}
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("A%d", i+4), jenis_source)
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("B%d", i+4), each["p"])
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("C%d", i+4), each["t"])
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("D%d", i+4), each["o"])
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("E%d", i+4), each["total"])
-	// }
-
-	// rowCountKdUser := 8
-	// awalRowCountKdUser := 8
-	// for _, each := range rekapKdUser {
-	// 	user := s.uR.FindByUsername(each["kd_user"].(string))
-	// 	if user.ID == 0 {
-	// 		continue
-	// 	}
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("A%d", rowCountKdUser), user.Username)
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("B%d", rowCountKdUser), user.Name)
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("C%d", rowCountKdUser), each["p"])
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("D%d", rowCountKdUser), each["t"])
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("E%d", rowCountKdUser), each["o"])
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("F%d", rowCountKdUser), each["total"])
-	// 	xlsx.SetCellValue(rekapSheet, fmt.Sprintf("G%d", rowCountKdUser), user.DataSource)
-	// 	rowCountKdUser += 1
-	// }
-	// xlsx.SetCellStyle(rekapSheet, fmt.Sprintf("A%d", awalRowCountKdUser), fmt.Sprintf("G%d", rowCountKdUser-1), styleBorder)
-
-	// rowCountRekapByAlasanPending := 4
-	// awalRowCountRekapByAlasanPending := 4
-	// rekapByAlasanPending := s.trR.RekapByAlasanPending(tgl1, tgl2)
-	// for _, each := range rekapByAlasanPending {
-	// 	xlsx.SetCellValue(pendingSheet, fmt.Sprintf("A%d", rowCountRekapByAlasanPending), each["alasan"])
-	// 	xlsx.SetCellValue(pendingSheet, fmt.Sprintf("B%d", rowCountRekapByAlasanPending), each["total"])
-	// 	rowCountRekapByAlasanPending += 1
-	// }
-	// xlsx.SetCellStyle(pendingSheet, fmt.Sprintf("A%d", awalRowCountRekapByAlasanPending), fmt.Sprintf("B%d", rowCountRekapByAlasanPending-1), styleBorder)
-
-	// listCol := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I"}
-	// rowCountRekapByAlasanPending += 1
-	// xlsx.SetCellValue(pendingSheet, fmt.Sprintf("A%d", rowCountRekapByAlasanPending), "Id User")
-	// xlsx.SetCellValue(pendingSheet, fmt.Sprintf("B%d", rowCountRekapByAlasanPending), "Nama")
-	// xlsx.SetCellValue(pendingSheet, fmt.Sprintf("C%d", rowCountRekapByAlasanPending), "Tidak Ada Alasan")
-	// xlsx.SetCellStyle(pendingSheet, fmt.Sprintf("A%d", rowCountRekapByAlasanPending), fmt.Sprintf("%s%d", listCol[len(masterPending)+2], rowCountRekapByAlasanPending), headerStyle)
-	// for i, v := range masterPending {
-	// 	xlsx.SetCellValue(pendingSheet, fmt.Sprintf("%s%d", listCol[i+3], rowCountRekapByAlasanPending), v.Nama)
-	// 	xlsx.SetCellValue(pendingSheet, fmt.Sprintf("%s%d", listCol[i+3], rowCountRekapByAlasanPending), v.Nama)
-	// 	xlsx.SetColWidth(pendingSheet, listCol[i+3], listCol[i+3], float64(len(v.Nama))+4)
-	// }
-	// for _, v := range rincianPending {
-	// 	user := s.uR.FindByUsername(v["kd_user"].(string))
-	// 	if user.ID == 0 {
-	// 		continue
-	// 	}
-	// 	xlsx.SetCellValue(pendingSheet, fmt.Sprintf("A%d", rowCountRekapByAlasanPending+1), v["kd_user"])
-	// 	xlsx.SetCellValue(pendingSheet, fmt.Sprintf("B%d", rowCountRekapByAlasanPending+1), user.Name)
-	// 	xlsx.SetCellValue(pendingSheet, fmt.Sprintf("C%d", rowCountRekapByAlasanPending+1), v["kosong"])
-	// 	for j, vj := range masterPending {
-	// 		xlsx.SetCellValue(pendingSheet, fmt.Sprintf("%s%d", listCol[j+3], rowCountRekapByAlasanPending+1), v[fmt.Sprintf("%d", vj.Id)])
-	// 	}
-	// 	rowCountRekapByAlasanPending += 1
-	// }
-
-	// rowCountRekapByAlasanTdkBerminat := 4
-	// awalRowCountRekapByAlasanTdkBerminat := 4
-	// rekapByAlasanTdkBerminat := s.trR.RekapByAlasanTdkBerminat(tgl1, tgl2)
-	// for _, each := range rekapByAlasanTdkBerminat {
-	// 	xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("A%d", rowCountRekapByAlasanTdkBerminat), each["alasan"])
-	// 	xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("B%d", rowCountRekapByAlasanTdkBerminat), each["total"])
-	// 	rowCountRekapByAlasanTdkBerminat += 1
-	// }
-	// xlsx.SetCellStyle(tdkBerminatSheet, fmt.Sprintf("A%d", awalRowCountRekapByAlasanTdkBerminat), fmt.Sprintf("B%d", rowCountRekapByAlasanTdkBerminat-1), styleBorder)
-
-	// rowCountRekapByAlasanTdkBerminat += 1
-	// xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("A%d", rowCountRekapByAlasanTdkBerminat), "Id User")
-	// xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("B%d", rowCountRekapByAlasanTdkBerminat), "Nama")
-	// xlsx.SetCellStyle(tdkBerminatSheet, fmt.Sprintf("A%d", rowCountRekapByAlasanTdkBerminat), fmt.Sprintf("%s%d", listCol[len(masterAlasanTdkBerminat)+1], rowCountRekapByAlasanTdkBerminat), headerStyle)
-	// for i, v := range masterAlasanTdkBerminat {
-	// 	xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("%s%d", listCol[i+2], rowCountRekapByAlasanTdkBerminat), v.Nama)
-	// 	xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("%s%d", listCol[i+2], rowCountRekapByAlasanTdkBerminat), v.Nama)
-	// 	xlsx.SetColWidth(tdkBerminatSheet, listCol[i+2], listCol[i+2], float64(len(v.Nama))+4)
-	// }
-	// for _, v := range rincianTdkBerminat {
-	// 	user := s.uR.FindByUsername(v["kd_user"].(string))
-	// 	if user.ID == 0 {
-	// 		continue
-	// 	}
-	// 	xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("A%d", rowCountRekapByAlasanTdkBerminat+1), v["kd_user"])
-	// 	xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("B%d", rowCountRekapByAlasanTdkBerminat+1), user.Name)
-	// 	for j, vj := range masterAlasanTdkBerminat {
-	// 		xlsx.SetCellValue(tdkBerminatSheet, fmt.Sprintf("%s%d", listCol[j+2], rowCountRekapByAlasanTdkBerminat+1), v[fmt.Sprintf("%d", vj.Id)])
-	// 	}
-	// 	rowCountRekapByAlasanTdkBerminat += 1
-	// }
-
-	err := xlsx.SaveAs("./file-report-asuransi.xlsx")
+	err = xlsx.SaveAs("./file-report-asuransi.xlsx")
 	if err != nil {
 		fmt.Println(err)
 	}
