46909cd1559ae21d13254b00cab040d8da836a7f
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2024-11-01 15:15:01 +0700

    benerin fakturr3

--- entity/data_wa_blast.go
+++ entity/data_wa_blast.go
@@ -3,7 +3,7 @@ package entity
 type DataWaBlast struct {
 	NoMsn            string
 	NmCustomer       string
-	KdUser           string
+	KdUser           *string
 	NoYgDiHubRenewal *string
 	NoWa             string
 	TglAkhirTenor    string
