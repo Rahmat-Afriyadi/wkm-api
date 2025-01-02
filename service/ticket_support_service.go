f952c4d6fa1b7f8af566d2a02b0b82356ac58198
KenzieL16 <anandabagas722@gmail.com>
2025-01-02 13:47:06 +0700

    benerin api tiket

--- service/ticket_support_service.go
+++ service/ticket_support_service.go
@@ -8,11 +8,12 @@ import (
 
 type TicketSupportService interface {
 	CreateTicketSupport(data request.TicketRequest, username string, tier uint32) (string, string, error)
-	EditTicketSupport(noTicket string, data request.TicketRequest, username string) (string, error)
+	EditTicketSupport(noTicket string, data request.TicketRequest, username string, role uint32) (string, string, error)
 	ViewTicketSupport(noTicket string) (entity.TicketSupport, error)
 	ListTicketUser(username string) ([]entity.TicketSupport, error)
 	ListTicketIT(username string) ([]entity.TicketSupport, error)
 	ListTicketQueue(month string, year string) ([]entity.TicketSupport, error)
+	// ExportDataTicketSupport(month string, year string) ([]entity.TicketSupport, error)
 }
 
 type ticketSupportService struct {
@@ -26,28 +27,28 @@ func NewTicketSupportService(tR repository.TicketSupportRepository) TicketSuppor
 }
 
 // CreateTicketSupport handles the business logic for creating a ticket.
-func (s *ticketSupportService) CreateTicketSupport(data request.TicketRequest, username string, tier uint32) (string,string, error) {
-	// Panggil repository untuk membuat ticket dan dapatkan noTicket dan error
+func (s *ticketSupportService) CreateTicketSupport(data request.TicketRequest, username string, tier uint32) (string, string, error) {
+	// Call repository to create ticket and get noTicket and error
 	noTicket, assignResult, err := s.tsR.CreateTicketSupport(data, username, tier)
 	if err != nil {
-		// Jika ada error, kembalikan error tersebut
-		return "","", err
+		// Return error if exists
+		return "", "", err
 	}
 
-	// Kembalikan noTicket jika sukses
-	return noTicket,assignResult, nil
+	// Return noTicket if successful
+	return noTicket, assignResult, nil
 }
 
-func (s *ticketSupportService) EditTicketSupport(noTicket string, data request.TicketRequest, username string) (string, error) {
-    // Panggil repository untuk edit ticket dan dapatkan error
-    noTicket, err := s.tsR.EditTicketSupport(noTicket, data, username)
-    if err != nil {
-        // Jika ada error, kembalikan error tersebut
-        return "", err
-    }
+func (s *ticketSupportService) EditTicketSupport(noTicket string, data request.TicketRequest, username string, role uint32) (string, string, error) {
+	// Call repository to edit ticket and get error
+	noTicket, err := s.tsR.EditTicketSupport(noTicket, data, username, role)
+	if err != nil {
+		// Return error if exists
+		return "", "", err
+	}
 
-    // Kembalikan nil jika sukses
-    return noTicket, nil
+	// Return nil if successful
+	return noTicket, "Ticket updated successfully", nil
 }
 
 func (s *ticketSupportService) ViewTicketSupport(noTicket string) (entity.TicketSupport, error) {
@@ -81,3 +82,9 @@ func (s *ticketSupportService) ListTicketIT(username string) ([]entity.TicketSup
 	}
 	return ticket, nil
 }
+
+// func (s *ticketSupportService) ExportDataTicketSupport(month string, year string) ([]entity.TicketSupport, error) {
+// 	ticket, err := s.tsR.ExportDataTicketSupport(month, year)
+// 	if err != nil {
+// 		return nil,
+	
